081a5f12c0e24def9b318e62d19fd8045bd0e5f2
James Reggio <james.reggio@gmail.com>
2016-07-30 17:10:50 -0400

    Respect `controls` prop on non-fullscreen video (iOS implementation)

--- RCTVideo.m
+++ RCTVideo.m
@@ -87,7 +87,7 @@ static NSString *const playbackRate = @"rate";
 
 - (AVPlayerViewController*)createPlayerViewController:(AVPlayer*)player withPlayerItem:(AVPlayerItem*)playerItem {
     RCTVideoPlayerViewController* playerLayer= [[RCTVideoPlayerViewController alloc] init];
-    playerLayer.showsPlaybackControls = NO;
+    playerLayer.showsPlaybackControls = _controls;
     playerLayer.rctDelegate = self;
     playerLayer.view.frame = self.bounds;
     playerLayer.player = _player;
